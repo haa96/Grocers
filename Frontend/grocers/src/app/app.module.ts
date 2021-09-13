f159fed990c99c8ff179383b31fb40932ebd8865
haa96 <54597354+haa96@users.noreply.github.com>
2021-09-13 15:00:19 -0500

    Update app.module.ts

--- Frontend/grocers/src/app/app.module.ts
+++ Frontend/grocers/src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from  '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { UserComponent } from './user/user.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AdminComponent } from './admin/admin.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTreeModule} from '@angular/material/tree';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    EmployeeComponent,
+    AdminComponent,
+    RegisterComponent
   ],
+ 
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatGridListModule,
+    MatTreeModule,
     BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
