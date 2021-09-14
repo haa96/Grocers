5343fc77ec1604ee15e8fde0aa58a7566b9fafb2
tylertoussaint <tjtoussaint@gmail.com>
2021-09-14 13:53:31 -0400

    Create empmain.component.ts

---
+++ Frontend/grocers/src/app/employee/empmain/empmain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-main',
+  templateUrl: './empmain.component.html',
+  styleUrls: ['./empmain.component.css']
+})
+export class EmpMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
