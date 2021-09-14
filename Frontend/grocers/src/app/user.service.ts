774843c5128ce58f5658d2187b3cd75474c6ba4f
AMahaffey <amahaffey1@gmail.com>
2021-09-13 21:13:05 -0700

    Update user.service.ts
    import name fix from UserLogin to Userlogin

--- Frontend/grocers/src/app/user.service.ts
+++ Frontend/grocers/src/app/user.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { UserComponent } from './user/user.component';
 import { Observable } from 'rxjs';
-import { UserLogin } from './userlogin';
+import { Userlogin } from './userlogin';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor(public http:HttpClient) { }
-  checkUserDetails(login:UserLogin):Observable<any>{
+  checkUserDetails(login:Userlogin):Observable<any>{
     return this.http.post("http://localhost:9090/api/user/login",login,
     {responseType:'text'});
   }
