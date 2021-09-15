70ed6f4760ddc3538bec21eaebaf34ea02daaca4
haa96 <54597354+haa96@users.noreply.github.com>
2021-09-14 21:14:33 -0500

    routing stuff

--- Frontend/grocers/src/app/app-routing.module.ts
+++ Frontend/grocers/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent} from'./cart/cart.component';
 import { HomeComponent} from'./home/home.component';
+import { AdminpanelComponent} from'./admin/adminpanel/adminpanel.component';
 import { ProfileComponent } from './user/profile/profile.component';
 import { MainComponent } from './user/main/main.component';
-
+import { ReportComponent } from './admin/report/report.component';
 const routes: Routes = [
+  {path:"AdminPan",component:AdminpanelComponent},
+  {path:"report",component:ReportComponent},
   {path:"profile",component:ProfileComponent},
+  {path:"main",component:MainComponent},
   {path:"cart",component:CartComponent},
-  {path:"",component:HomeComponent},
-  {path:"main",component:MainComponent}
+  {path:"",component:HomeComponent}
+
 ];
 
 @NgModule({
