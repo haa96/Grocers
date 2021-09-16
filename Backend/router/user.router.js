bbba98081ee9b85a6a4eeba5f47486c7246f42ee
haa96 <54597354+haa96@users.noreply.github.com>
2021-09-16 12:56:26 -0500

    udate info progress

--- Backend/router/user.router.js
+++ Backend/router/user.router.js
@@ -8,4 +8,6 @@ console.log("In User Router");
 router.post("/userlogin", userController.userlogin);
 router.post("/userregister", userController.register);
 router.get("/getUser", userController.getUserInfo);
+router.put("/updateUser",userController.updateUserInfo);
+
 module.exports=router;
