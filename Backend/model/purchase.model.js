109a6f777bb8fa64b4ad940c3828bb9082cf7299
AhmedBastawi1 <87991309+AhmedBastawi1@users.noreply.github.com>
2021-09-16 16:44:32 -0400

    done

--- Backend/model/purchase.model.js
+++ Backend/model/purchase.model.js
@@ -3,19 +3,12 @@ let mongoose = require("mongoose");
 mongoose.pluralize(null);
 
 let purchaseSchema = mongoose.Schema({
-    // product ID
-    pID : Number,
+
     // customer ID
     cID : Number,
-    // quantity of product purchased
-    qty : Number,
-    // the standard product price
+    // the standard product total price
     price : Number,
     // the discount on the product
-    discount : Number,
-    // the date purchased
-    date : Date,
-    
     orderStatus : String
 });
 
