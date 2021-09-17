95ccb2094b1dd94a71208fc79b07e83a283a2ef8
AhmedBastawi1 <87991309+AhmedBastawi1@users.noreply.github.com>
2021-09-17 08:13:27 -0400

    AddtoCart

--- Frontend/grocers/src/app/user/main/main.component.ts
+++ Frontend/grocers/src/app/user/main/main.component.ts
@@ -15,25 +15,26 @@ import { UserService } from 'src/app/user.service';
 export class MainComponent implements OnInit {
   products = [];
   item?: Array<Citem> = [];
-carts: Array<Citem> = [];
+  carts: Array<Citem> = [];
   counterqty = 1;
   total: number;
   i = 0;
-  constructor(public ser: SharedService, public router: Router, public userSer:UserService,
-     public adminSer: AdminService, public activateRoute: ActivatedRoute) { }
-     msg?:string;
-     userEmail?:string;
-     firstName?:string;
-     lastName?:string;
-     email:string = "";
-     balance?:number;
+  constructor(public ser: SharedService, public router: Router, public userSer: UserService,
+    public adminSer: AdminService, public activateRoute: ActivatedRoute) { }
+  msg?: string;
+  userEmail?: string;
+  firstName?: string;
+  lastName?: string;
+  email: string = "";
+  balance?: number;
   ngOnInit(): void {
-    this.activateRoute.params.subscribe(data=>this.userEmail=data.user);
+    this.activateRoute.params.subscribe(data => this.userEmail = data.user);
     this.getUser();
     this.populateProducts();
   }
-  cart() { this.router.navigate(["cart",this.userEmail]); }
-  profile() { this.router.navigate(["profile",this.userEmail]); }
+  cart() { this.router.navigate(["cart", this.userEmail]); }
+  profile() { this.router.navigate(["profile", this.userEmail]); }
+  //Get Product 
   populateProducts() {
     this.adminSer.getproductDetails().
       subscribe(data => {
@@ -43,30 +44,32 @@ carts: Array<Citem> = [];
         console.log(this.products);
       }, error => console.error(error));
   }
+//get username info
   getUser() {
     console.log(this.userEmail);
     this.userSer.getUserDetails(this.userEmail).
-    subscribe(result=>{
-      console.log(result);
-      this.firstName = result.firstName;
-      this.lastName = result.lastName;
-      this.email=result.email;
-      this.balance=result.balance;
-      console.log(this.firstName+this.email);
-    }, error=>console.log(error));
+      subscribe(result => {
+        console.log(result);
+        this.firstName = result.firstName;
+        this.lastName = result.lastName;
+        this.email = result.email;
+        this.balance = result.balance;
+        console.log(this.firstName + this.email);
+      }, error => console.log(error));
   }
-addcart(name: any, price: any, quantity: any) {
-      this.total = price * quantity;
-      let ite: Citem = { id: this.i, name: name, price: price, Qty: quantity, total: this.total };
-      console.log(ite)
-      this.carts.push(ite);
-      this.ser.setCartArray(this.carts);
-      this.i++
-      this.item = this.ser.getCartArray()
-      console.log(name)
+  //Add products to the card 
+  addcart(name: any, price: any, quantity: any) {
+    this.total = price * quantity;
+    let ite: Citem = { id: this.i, name: name, price: price, Qty: quantity, total: this.total };
+    console.log(ite)
+    this.carts.push(ite);
+    this.ser.setCartArray(this.carts);
+    this.i++
+    this.item = this.ser.getCartArray()
+    console.log(name)
+  }
 
-
-      }
-
-      Signout(){this.router.navigate([""]);}
-    }
+  Signout() {
+    this.router.navigate([""]);
+  }
+}
